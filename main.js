98ce2ca5717b8b2396ed948e565de4c27dc81a96
DucThien <phamthienduc069@gmail.com>
2023-01-13 17:08:20 +0700

    update

--- main.js
+++ main.js
@@ -1,35 +1,33 @@
 document.querySelector('#add').onclick = function() {
+
+// Nếu ko có giá trị hoặc giá trị rỗng sẽ hiện alert
     if(document.querySelector('.input input').value == 0) {
         alert('Please Enter The Text!!') 
     }
-console.log("alert");
-    // else {
-    //     document.querySelector("output").innerHTML += `
-    //     <div class = "tag">
-    //         <div class = "tag_text">    
-    //             ${document.querySelector(".input input").value}
-    //         </div>
-    //         <div class = "tag_btn">
-    //             <button class = "up">
-    //                 <i class="fa-solid fa-circle-chevron-up"></i>
-    //             </button>
-    //             <button class = "delete">
-    //                 <i class="fa-solid fa-circle-xmark"></i>    
-    //             </button>
-    //             <button class = "down"> 
-    //                 <i class="fa-solid fa-circle-chevron-down"></i>
-    //             </button>
-    //         </div>
-    //     </div>
-    //     `;
-        
-    //     var but = document.querySelectorAll(".delete");
+// Nếu có giá trị sẽ in ra nội dung 
+    else {
+        document.querySelector(".output").innerHTML += `
+        <div class = "tag">
+            <span class = "tag_input">        
+                ${document.querySelector(".input input").value}
+            </span>
+                <button class = "upItemBtn">
+                    <i class="fa-solid fa-circle-chevron-up"></i>
+                </button>
+                <button class = "removeItemBtn">
+                    <i class="fa-solid fa-circle-xmark"></i>    
+                </button>
+                <button class = "downItemBtn"> 
+                    <i class="fa-solid fa-circle-chevron-down"></i>
+                </button>
+        </div>
+        `;
 
-    //     for(var i = 0; i < but.length; i++) {
-    //         but[i].onclick = function(){
-    //             this.parentNode.remove();
-    //         }
-    //     }
-
-    // }
+        var but = document.querySelectorAll(".removeItemBtn");
+        for(var i = 0; i < but.length; i++) {
+            but[i].onclick = function(){
+                this.parentNode.remove();
+            }
+        }
+    }
 }
