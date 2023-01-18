cf526fc1a3f7334ffe8790287e537b519e815481
DucThien <phamthienduc069@gmail.com>
2023-01-19 00:25:53 +0700

    firt commit

--- main.js
+++ main.js
@@ -1,33 +1,83 @@
-document.querySelector('#add').onclick = function() {
+var inputVal = document.querySelector('#item');
+var addTaskBtn = document.querySelector('#add');
+var outPut = document.querySelector('.output');
+var hexString = "0123456789abcdef";
+// var taskOutput = document.querySelectorAll('.tasks');
 
-// Nếu ko có giá trị hoặc giá trị rỗng sẽ hiện alert
-    if(document.querySelector('.input input').value == 0) {
-        alert('Please Enter The Text!!') 
-    }
-// Nếu có giá trị sẽ in ra nội dung 
-    else {
-        document.querySelector(".output").innerHTML += `
-        <div class = "tag">
-            <span class = "tag_input">        
-                ${document.querySelector(".input input").value}
-            </span>
-                <button class = "upItemBtn">
-                    <i class="fa-solid fa-circle-chevron-up"></i>
-                </button>
-                <button class = "removeItemBtn">
-                    <i class="fa-solid fa-circle-xmark"></i>    
-                </button>
-                <button class = "downItemBtn"> 
-                    <i class="fa-solid fa-circle-chevron-down"></i>
-                </button>
-        </div>
-        `;
 
-        var but = document.querySelectorAll(".removeItemBtn");
-        for(var i = 0; i < but.length; i++) {
-            but[i].onclick = function(){
-                this.parentNode.remove();
-            }
-        }
-    }
+// Gắn sự kiện cho nút
+addTaskBtn.addEventListener('click', function () {
+   if(inputVal.value == 0) {alert('Please Enter Somthing!!')};
+   if(inputVal.value != 0) {
+      var localItems = JSON.parse( localStorage.getItem('localItem'));
+        if(localItems === null){
+            taskList = []
+   } 
+   else {
+       taskList = localItems;
 }
+
+// Truyền giá trị phần input vào task
+   taskList.push(inputVal.value)
+// Clear input after add content
+   inputVal.value = " "
+// Set Value cho local
+   localStorage.setItem('localItem', JSON.stringify(taskList)); 
+}
+   showItem()
+})
+
+// Lưu trên local storage
+function showItem(){
+   var localItems = JSON.parse( localStorage.getItem('localItem'))
+   if(localItems === null){
+    taskList = []
+     
+   } else {
+       taskList = localItems;
+   }
+
+// Hiển thị task
+var html = '';
+var itemShowAll = document.querySelector('.output');
+taskList.forEach((data, index ) => {
+    html += ` 
+   <div class="tasks">
+   <span class="pText">${data}</span>
+   <button class="deleteTask" onClick="deleteItem(${index})">
+    <i class="fa-solid fa-circle-xmark"></i>  
+   </button>
+   </div>
+   `
+})
+itemShowAll.innerHTML = html;
+}
+
+// Xóa task
+showItem()
+function deleteItem(index){
+   var localItems = JSON.parse( localStorage.getItem('localItem'))
+   taskList.splice(index, 1)
+   localStorage.setItem('localItem', JSON.stringify(taskList));
+   showItem()
+};
+
+// Change task color
+// var randomColor = () => {
+//    var hexCode = "#";
+//    for( i = 0; i < 6; i++){
+//        hexCode += hexString[Math.floor(Math.random() * hexString.length)];
+//    }
+//    return hexCode;
+// }
+
+// var generateGrad = () => {
+//    var colorOne = randomColor();
+//    var colorTwo = randomColor();
+//    var direction = Math.floor(Math.random() * 360);
+//    outPut.style.background = 
+//    `linear-gradient(${direction}deg, ${colorOne}, ${colorTwo})`;
+   
+// }
+
+// addTaskBtn.addEventListener("click", generateGrad);
